266d0bdc2376492b2f8852793a6730476bf5d514
parsa <parsafarashah@gmail.com>
2025-04-19 17:37:29 +0330

    button main 2

--- app/components/ButtonMain.tsx
+++ app/components/ButtonMain.tsx
@@ -8,7 +8,7 @@ type Ttitle = {
 function ButtonMain({ image, text }: Ttitle) {
   return (
     <div>
-      <button className="w-auto flex justify-center  items-center h-10 text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:outline-none  shadow-lg shadow-blue-500/50 dark:shadow-md dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 duration-600 ">
+      <button className="w-auto flex justify-center  items-center h-10 text-white bg-gradient-to-r from-blue-500 via-blue-600 border-1 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 ">
         {image && (
           <Image
             src={image}
