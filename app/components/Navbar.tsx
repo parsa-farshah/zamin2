38f6531a513ee4f387beb9f41341e224d4a2030a
parsa <parsafarashah@gmail.com>
2025-04-21 19:11:05 +0330

    haeder finish without fonts 2

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,7 +1,6 @@
 import Image from "next/image";
 import React from "react";
 import logoZamin from "../../public/images/logoZamin.svg";
-import ButtonMain from "./ButtonMain";
 
 function Navbar() {
   return (
