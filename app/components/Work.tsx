ecdb0dbe8631c11a48744df518405bb8820f5e99
parsa <parsafarashah@gmail.com>
2025-04-24 16:30:37 +0330

    work3

--- app/components/Work.tsx
+++ app/components/Work.tsx
@@ -1,8 +1,9 @@
 import Image from "next/image";
 import React from "react";
 import dotWork from "../../public/images/dotsWork.svg";
-import imageWork from "../../public/images/ImageWork.svg";
+import imageWork from "../../public/images/imageWork.svg";
 import dotBlu from "../../public/images/dotBlu.svg";
+import younes from "../../public/images/younesImage.png";
 function Work() {
   return (
     <div className="flex flex-col justify-center pt-[100px]">
@@ -10,10 +11,10 @@ function Work() {
         <div className="bg-[#EAEAEA] w-full h-0.5"></div>
         <div className="flex gap-3">
           <div className="w-full flex justify-center">
-            <button className="w-[131px] flex justify-around items-center h-9 bg-[#EBEBEB] font-semibold rounded-lg text-sm text-center font-inter">
-              <Image src={dotWork} alt="" />
+            <button className="w-[131px] flex justify-center rounded-[12px] items-center h-9 bg-[#EBEBEB] font-semibold  text-sm text-center font-inter ">
+              <Image className="w-[17px] mx-2" src={dotWork} alt="" />
               Work
-              <Image src={dotWork} alt="" />
+              <Image className="w-[17px] mx-2" src={dotWork} alt="" />
             </button>
           </div>
         </div>
@@ -27,13 +28,151 @@ function Work() {
           Discover the magic we’ve crafted lately- wher <br /> creativiti meets
           impact see what we;wve been <br /> up to and get inspired
         </p>
+        {/* image section */}
         <div className="pt-11">
-          <Image className="object-cover" src={imageWork} alt="image work" />
-          <Image
-            className="object-cover pt-11"
-            src={imageWork}
-            alt="image work"
-          />
+          <div className="pt-11">
+            <Image className="object-cover" src={imageWork} alt="image work" />
+            {/* under image text */}
+            <div className="flex justify-between sm:justify-around items-center w-full h-[57px]">
+              <div>
+                <h3 className="font-inter font-bold text-[10px] text-[#C3C3C3] pt-4">
+                  PROJECT NAME
+                </h3>
+                <p className="font-inter font-normal text-[14px] text-[#464646] pt-4">
+                  Bluegen v1.2
+                </p>
+              </div>
+              <div className="hidden sm:block">
+                <h3 className="font-inter font-bold text-[10px] text-[#C3C3C3] pt-4">
+                  Budget
+                </h3>
+                <p className="font-inter font-normal text-[14px] text-[#464646] pt-4">
+                  800$ - 1200$
+                </p>
+              </div>
+              <div className="hidden sm:block">
+                <h3 className="font-inter font-bold text-[10px] text-[#C3C3C3] pt-4">
+                  Technology
+                </h3>
+                <p className="font-inter font-normal text-[14px] text-[#464646] pt-4">
+                  Figma - next js
+                </p>
+              </div>
+              <div className="pt-5">
+                <h3 className="font-inter font-bold text-[10px] text-[#C3C3C3]">
+                  Members
+                </h3>
+                {/* 4image  */}
+                <div className="flex  items-start pt-1 sm:pt-1">
+                  <div className="z-30 bg-amber-800 w-[36px] h-[36px] rounded-[24px] border-2 border-[#F5F5F5]">
+                    <Image
+                      className="rounded-2xl"
+                      alt="imageYounes"
+                      src={younes}
+                      layout="responsive"
+                    />
+                  </div>
+                  <div className="z-20 relative left-[-15px] bg-amber-800 w-[36px] h-[36px] rounded-[24px] border-2 border-[#F5F5F5]">
+                    <Image
+                      className="rounded-2xl"
+                      src={younes}
+                      alt="imageYounes"
+                      layout="responsive"
+                    />
+                  </div>
+                  <div className="z-10 relative left-[-30px] bg-amber-800 w-[36px] h-[36px] rounded-[24px] border-2 border-[#F5F5F5]">
+                    <Image
+                      className="rounded-2xl"
+                      src={younes}
+                      alt="imageYounes"
+                      layout="responsive"
+                    />
+                  </div>
+                  <div className="z-0 relative left-[-45px] bg-amber-800 w-[36px] h-[36px] rounded-[24px] border-2 border-[#F5F5F5]">
+                    <Image
+                      className="rounded-2xl"
+                      src={younes}
+                      alt="imageYounes"
+                      layout="responsive"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+            {/* end under image */}
+          </div>
+          {/* image 2 */}
+          <div className="pt-11">
+            <Image className="object-cover" src={imageWork} alt="image work" />
+            {/* under image text */}
+            <div className="flex justify-between sm:justify-around items-center w-full h-[57px]">
+              <div>
+                <h3 className="font-inter font-bold text-[10px] text-[#C3C3C3] pt-4">
+                  PROJECT NAME
+                </h3>
+                <p className="font-inter font-normal text-[14px] text-[#464646] pt-4">
+                  Bluegen v1.2
+                </p>
+              </div>
+              <div className="hidden sm:block">
+                <h3 className="font-inter font-bold text-[10px] text-[#C3C3C3] pt-4">
+                  Budget
+                </h3>
+                <p className="font-inter font-normal text-[14px] text-[#464646] pt-4">
+                  800$ - 1200$
+                </p>
+              </div>
+              <div className="hidden sm:block">
+                <h3 className="font-inter font-bold text-[10px] text-[#C3C3C3] pt-4">
+                  Technology
+                </h3>
+                <p className="font-inter font-normal text-[14px] text-[#464646] pt-4">
+                  Figma - next js
+                </p>
+              </div>
+              <div className="pt-5">
+                <h3 className="font-inter font-bold text-[10px] text-[#C3C3C3]">
+                  Members
+                </h3>
+                {/* 4image  */}
+                <div className="flex  items-start pt-1 sm:pt-1">
+                  <div className="z-30 bg-amber-800 w-[36px] h-[36px] rounded-[24px] border-2 border-[#F5F5F5]">
+                    <Image
+                      className="rounded-2xl"
+                      alt="imageYounes"
+                      src={younes}
+                      layout="responsive"
+                    />
+                  </div>
+                  <div className="z-20 relative left-[-15px] bg-amber-800 w-[36px] h-[36px] rounded-[24px] border-2 border-[#F5F5F5]">
+                    <Image
+                      className="rounded-2xl"
+                      src={younes}
+                      alt="imageYounes"
+                      layout="responsive"
+                    />
+                  </div>
+                  <div className="z-10 relative left-[-30px] bg-amber-800 w-[36px] h-[36px] rounded-[24px] border-2 border-[#F5F5F5]">
+                    <Image
+                      className="rounded-2xl"
+                      src={younes}
+                      alt="imageYounes"
+                      layout="responsive"
+                    />
+                  </div>
+                  <div className="z-0 relative left-[-45px] bg-amber-800 w-[36px] h-[36px] rounded-[24px] border-2 border-[#F5F5F5]">
+                    <Image
+                      className="rounded-2xl"
+                      src={younes}
+                      alt="imageYounes"
+                      layout="responsive"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+            {/* end under image */}
+          </div>
         </div>
       </div>
       <div className="pt-14">
@@ -41,9 +180,10 @@ function Work() {
           <div className="bg-[#EAEAEA] w-full h-0.5"></div>
           <div className="flex gap-3">
             <div className="w-full flex justify-center">
-              <button className="w-[138px] flex justify-around text-sm items-center h-9 text-white bg-gradient-to-r from-gray-950 via-gray-800 to-gray-950 hover:bg-gradient-to-br shadow-md shadow-gray-100/20 dark:shadow-md dark:shadow-gray-300/80 font-medium rounded-lg text-center font-inter ">
-                <Image src={dotBlu} alt="dot blue" />
-                See more Works
+              <button className="w-[138px] flex justify-start text-sm items-center h-9 text-white bg-gradient-to-r from-gray-950 via-gray-800 to-gray-950 hover:bg-gradient-to-br shadow-md shadow-gray-100/20 dark:shadow-md dark:shadow-gray-300/80 font-medium rounded-lg text-center font-inter">
+                <Image className="ml-3" src={dotBlu} alt="dot blue" />
+
+                <span className="ml-1.5">See more Works</span>
               </button>
             </div>
           </div>
